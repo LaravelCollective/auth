1820e89d037b3292e1984000187215739044fd96
Matt Lantz <mattlantz@gmail.com>
2020-09-10 08:30:43 -0400

    Because these updates were done on Laravel UI
    Well, one was a PR

--- src/AuthRouteMethods.php
+++ src/AuthRouteMethods.php
@@ -13,44 +13,47 @@ class AuthRouteMethods
     public function auth()
     {
         return function ($options = []) {
-            // Login Routes...
-            if ($options['login'] ?? true) {
-                $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-                $this->post('login', 'Auth\LoginController@login');
-            }
+           $namespace = class_exists($this->prependGroupNamespace('Auth\LoginController')) ? null : 'App\Http\Controllers';
 
-            // Logout Routes...
-            if ($options['logout'] ?? true) {
-                $this->post('logout', 'Auth\LoginController@logout')->name('logout');
-            }
+            $this->group(['namespace' => $namespace], function() use($options) {
+                // Login Routes...
+                if ($options['login'] ?? true) {
+                    $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+                    $this->post('login', 'Auth\LoginController@login');
+                }
 
-            // Registration Routes...
-            if ($options['register'] ?? true) {
-                $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-                $this->post('register', 'Auth\RegisterController@register');
-            }
+                // Logout Routes...
+                if ($options['logout'] ?? true) {
+                    $this->post('logout', 'Auth\LoginController@logout')->name('logout');
+                }
 
-            // Password Reset Routes...
-            if ($options['reset'] ?? true) {
-                $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-                $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-                $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-                $this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
-            }
+                // Registration Routes...
+                if ($options['register'] ?? true) {
+                    $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+                    $this->post('register', 'Auth\RegisterController@register');
+                }
 
-            // Password Confirmation Routes...
-            if ($options['confirm'] ??
-                class_exists($this->prependGroupNamespace('Auth\ConfirmPasswordController'))) {
+                // Password Reset Routes...
+                if ($options['reset'] ?? true) {
+                    $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+                    $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+                    $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+                    $this->post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+                }
+
+                // Password Confirmation Routes...
+                if ($options['confirm'] ?? class_exists($this->prependGroupNamespace('Auth\ConfirmPasswordController'))) {
                     $this->get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
-                $this->post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
-            }
+                    $this->post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
+                }
 
-            // Email Verification Routes...
-            if ($options['verify'] ?? false) {
-                $this->get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
-                $this->get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
-                $this->post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
-            }
+                // Email Verification Routes...
+                if ($options['verify'] ?? false) {
+                    $this->get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+                    $this->get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+                    $this->post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+                }
+            });
         };
     }
 }
